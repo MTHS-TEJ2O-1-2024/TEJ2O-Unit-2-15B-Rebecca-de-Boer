4f35f956f4c41f23c1230d2199b17bc7ade266d2
Rebecca-de-Boer <rebecca.de-boer@mths.ca>
2024-12-10 11:12:40 -0500

    made code

--- main.ts
+++ main.ts
@@ -1,8 +1,36 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: 
+ * Created on: dec 2024
+ * This program moves sprite in a square
 */
 
-basic.showString('Hello, World!')
+//variables 
+let sprite:game.LedSprite = null
+let loopbox = 0
+let loopside = 0
+
+input.onButtonPressed(Button.A, function () {
+    //setup
+    basic.pause(500)
+    basic.clearScreen()
+    loopbox = 0
+    loopside = 0 
+    sprite = game.createSprite(0, 0)
+    sprite.set(LedSpriteProperty.X,loopside)
+    //loop to move the sprite around
+    while(loopbox <= 3) {
+        while(loopside <= 5){
+            sprite.move(1)
+            loopside++
+            basic.pause(500)
+        }
+        sprite.turn(Direction.Right, 90)
+        loopside = 0
+        loopbox++
+    }
+    //reset
+    sprite.delete()
+    basic.showIcon(IconNames.Happy)
+    
+})
